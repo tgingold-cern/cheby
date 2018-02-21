1600b00273765f7b0ba262eb29b993f319f90a8d
Tristan Gingold <tristan.gingold@cern.ch>
2018-02-21 12:05:05 +0100

    Add argument parser.

--- proto/cheby/main.py
+++ proto/cheby/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+import argparse
 import parser
 import pprint
 import sprint
@@ -7,26 +8,34 @@ import layout
 
 
 def main():
-    if len(sys.argv) == 1:
-        print("error: no input file")
-        sys.exit(2)
-    else:
-        for f in sys.argv[1:]:
-            try:
-                t = parser.parse_yaml(f)
-            except parser.ParseException as e:
-                sys.stderr.write("{}:parse error: {}\n".format(f, e.msg))
-                sys.exit(2)
+    aparser = argparse.ArgumentParser(description='cheby utility')
+    aparser.add_argument('--pretty-print', action='store_true',
+                         help='display the input in YAML')
+    aparser.add_argument('--simple-print', action='store_true',
+                         help='display the layout with fields')
+    aparser.add_argument('--memmap-print', action='store_true',
+                         help='display the layout without fields')
+    aparser.add_argument('FILE', nargs='+')
 
-            try:
-                layout.layout_cheby(t)
-            except layout.LayoutException as e:
-                sys.stderr.write("{}:layout error: {}\n".format(f, e.msg))
-                sys.exit(2)
-            if False:
-                pprint.pprint_cheby(sys.stdout, t)
-            if True:
-                sprint.sprint_cheby(sys.stdout, t)
+    args = aparser.parse_args()
+    for f in args.FILE:
+        try:
+            t = parser.parse_yaml(f)
+        except parser.ParseException as e:
+            sys.stderr.write("{}:parse error: {}\n".format(f, e.msg))
+            sys.exit(2)
+
+        try:
+            layout.layout_cheby(t)
+        except layout.LayoutException as e:
+            sys.stderr.write("{}:layout error: {}\n".format(f, e.msg))
+            sys.exit(2)
+        if args.pretty_print:
+            pprint.pprint_cheby(sys.stdout, t)
+        if args.memmap_print:
+            sprint.sprint_cheby(sys.stdout, t, False)
+        if args.simple_print:
+            sprint.sprint_cheby(sys.stdout, t, True)
 
 
 if __name__ == '__main__':
