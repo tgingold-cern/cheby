460c204973c2938601467f2c1487e5ea48b9a457
Tristan Gingold <tristan.gingold@cern.ch>
2018-09-25 14:25:30 +0200

    tb: test wb submap

--- testfiles/tb/array1_axi4_tb.vhdl
+++ testfiles/tb/array1_axi4_tb.vhdl
@@ -4,6 +4,7 @@ end array1_axi4_tb;
 library ieee;
 use ieee.std_logic_1164.all;
 
+use work.wishbone_pkg.all;
 use work.axi4_tb_pkg.all;
 
 architecture behav of array1_axi4_tb is
@@ -13,6 +14,10 @@ architecture behav of array1_axi4_tb is
   signal wr_out  : t_axi4lite_write_master_out;
   signal rd_in   : t_axi4lite_read_master_in;
   signal rd_out  : t_axi4lite_read_master_out;
+
+  --  For sub1.
+  signal sub1_wb_in  : t_wishbone_slave_in;
+  signal sub1_wb_out : t_wishbone_slave_out;
 
   signal end_of_test : boolean := false;
 begin
@@ -37,7 +42,7 @@ begin
       areset_n   => rst_n,
       awvalid    => wr_out.awvalid,
       awready    => wr_in.awready,
-      awaddr     => wr_out.awaddr(5 downto 2),
+      awaddr     => wr_out.awaddr(12 downto 2),
       awprot     => "010",
       wvalid     => wr_out.wvalid,
       wready     => wr_in.wready,
@@ -48,7 +53,7 @@ begin
       bresp      => wr_in.bresp,
       arvalid    => rd_out.arvalid,
       arready    => rd_in.arready,
-      araddr     => rd_out.araddr(5 downto 2),
+      araddr     => rd_out.araddr(12 downto 2),
       arprot     => "010",
       rvalid     => rd_in.rvalid,
       rready     => rd_out.rready,
@@ -57,7 +62,47 @@ begin
 
       ram1_adr_i => (others => '0'),
       ram1_rd_i  => '0',
-      ram1_dat_o => open);
+      ram1_dat_o => open,
+
+      sub1_wb_cyc_o => sub1_wb_in.cyc,
+      sub1_wb_stb_o => sub1_wb_in.stb,
+      sub1_wb_adr_o => sub1_wb_in.adr(9 downto 0),
+      sub1_wb_sel_o => sub1_wb_in.sel,
+      sub1_wb_we_o  => sub1_wb_in.we,
+      sub1_wb_dat_o => sub1_wb_in.dat,
+      sub1_wb_ack_i => sub1_wb_out.ack,
+      sub1_wb_err_i => sub1_wb_out.err,
+      sub1_wb_rty_i => sub1_wb_out.rty,
+      sub1_wb_stall_i => sub1_wb_out.stall,
+      sub1_wb_dat_i => sub1_wb_out.dat);
+
+  b1: block
+  begin
+    sub1_wb_out.err <= '0';
+    sub1_wb_out.rty <= '0';
+    sub1_wb_out.stall <= '0';
+
+    process(clk)
+    begin
+      if rising_edge(clk) then
+        if (sub1_wb_in.cyc and sub1_wb_in.stb) = '1' then
+          if sub1_wb_in.adr(9) = '1' then
+            --  Discard write, read addr
+            sub1_wb_out.dat( 7 downto  0) <= sub1_wb_in.adr(7 downto 0);
+            sub1_wb_out.dat(15 downto  8) <= not sub1_wb_in.adr(7 downto 0);
+            sub1_wb_out.dat(23 downto 16) <= not sub1_wb_in.adr(7 downto 0);
+            sub1_wb_out.dat(31 downto 24) <= sub1_wb_in.adr(7 downto 0);
+          else
+            --  0.
+            sub1_wb_out.dat <= (others => '0');
+          end if;
+          sub1_wb_out.ack <= '1';
+        else
+          sub1_wb_out.ack <= '0';
+        end if;
+      end if;
+    end process;
+  end block;
 
   process
     variable v : std_logic_vector(31 downto 0);
@@ -77,6 +122,7 @@ begin
     axi4lite_read (clk, rd_out, rd_in, x"0000_0000", v);
     assert v = x"0000_abcd" severity error;
 
+    --  Testing memory
     report "Testing memory" severity note;
     axi4lite_write (clk, wr_out, wr_in, x"0000_0024", x"abcd_0001");
     axi4lite_write (clk, wr_out, wr_in, x"0000_002c", x"abcd_0203");
@@ -84,9 +130,18 @@ begin
     axi4lite_read (clk, rd_out, rd_in, x"0000_0024", v);
     assert v = x"abcd_0001" severity error;
 
-    wait until rising_edge(clk);
     axi4lite_read (clk, rd_out, rd_in, x"0000_002c", v);
     assert v = x"abcd_0203" severity error;
+
+    --  Testing WB
+    report "Testing wishbone" severity note;
+    axi4lite_write (clk, wr_out, wr_in, x"0000_1000", x"9876_5432");
+
+    axi4lite_read (clk, rd_out, rd_in, x"0000_1804", v);
+    assert v = x"01fe_fe01" severity error;
+
+    axi4lite_read (clk, rd_out, rd_in, x"0000_1004", v);
+    assert v = x"0000_0000" severity error;
 
     wait until rising_edge(clk);
 
