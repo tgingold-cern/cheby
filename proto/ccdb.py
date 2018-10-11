754e6fb07d5905268ee05d01067db3e250f267f2
Tristan Gingold <tristan.gingold@cern.ch>
2018-10-11 14:37:16 +0200

    Fix indentation.

--- proto/ccdb.py
+++ proto/ccdb.py
@@ -11,6 +11,7 @@ import cheby.layout
 
 connection = None
 
+
 class Buffer(object):
     def __init__(self):
         self.buffer = ''
@@ -20,32 +21,33 @@ class Buffer(object):
 
 
 def show(cmd_name):
-   cursor = connection.cursor()
+    cursor = connection.cursor()
 
-   print('files:')
-   cursor.execute("SELECT * from cheby_files")
-   print('desc: {}'.format(cursor.description))
-   for l in cursor:
+    print('files:')
+    cursor.execute("SELECT * from cheby_files")
+    print('desc: {}'.format(cursor.description))
+    for l in cursor:
         print(l)
 
-   print('namespace:')
-   cursor.execute("SELECT * from cheby_namespaces")
-   for l in cursor:
+    print('namespace:')
+    cursor.execute("SELECT * from cheby_namespaces")
+    for l in cursor:
         print(l)
 
 
 def list_namespaces(cmd_name):
-   cursor = connection.cursor()
-   cursor.execute("SELECT * FROM cheby_namespaces")
-   print("{:4} {:20} {:10} {}".format('nid', 'name', 'creator', 'description'))
-   print('------------------------------------------------------------------')
-   for nid, name, desc, creator in cursor:
-      print("{:4} {:20} {:10} {}".format(nid, name, creator, desc))
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM cheby_namespaces")
+    print("{:4} {:20} {:10} {}".format(
+        'nid', 'name', 'creator', 'description'))
+    print('------------------------------------------------------------------')
+    for nid, name, desc, creator in cursor:
+        print("{:4} {:20} {:10} {}".format(nid, name, creator, desc))
 
 
 def list_filenames(cmd_name):
-   cursor = connection.cursor()
-   cursor.execute(
+    cursor = connection.cursor()
+    cursor.execute(
       """SELECT
         cheby_files.file_id,
         cheby_files.file_name,
@@ -54,306 +56,315 @@ def list_filenames(cmd_name):
       FROM cheby_files
       INNER JOIN cheby_namespaces
         ON cheby_files.namespace_id = cheby_namespaces.namespace_id""")
-   print("{:4} {:20} {:16} {}".format('fid', 'filename', 'namespace', 'date'))
-   print('------------------------------------------------------------------')
-   for fid, filename, namespace, date in cursor.fetchall():
-      print("{:4} {:20} {:16} {}".format(fid, filename, namespace, date))
+    print("{:4} {:20} {:16} {}".format('fid', 'filename', 'namespace', 'date'))
+    print('------------------------------------------------------------------')
+    for fid, filename, namespace, date in cursor.fetchall():
+        print("{:4} {:20} {:16} {}".format(fid, filename, namespace, date))
 
 
 def list_includes(cmd_name):
-   cursor = connection.cursor()
-   cursor.execute(
+    cursor = connection.cursor()
+    cursor.execute(
       """SELECT source_file_id, included_file_id
       FROM cheby_file_inclusions""")
-   print("{:4} {:4}".format('fid', 'iid'))
-   print('------------')
-   for fid, iid in cursor.fetchall():
-      print("{:4} {:4}".format(fid, iid))
+    print("{:4} {:4}".format('fid', 'iid'))
+    print('------------')
+    for fid, iid in cursor.fetchall():
+        print("{:4} {:4}".format(fid, iid))
 
 
 def remove_namespace(cmd_name, *args):
-   if len(args) != 1:
-      sys.stderr.write('usage: {} namespace\n'.format(cmd_name))
-      return
-   name = args[0]
-   cursor = connection.cursor()
-   cursor.execute(
-      "DELETE FROM cheby_namespaces WHERE namespace_name = '{}'".format(name))
-   connection.commit()
+    if len(args) != 1:
+        sys.stderr.write('usage: {} namespace\n'.format(cmd_name))
+        return
+    name = args[0]
+    cursor = connection.cursor()
+    cursor.execute(
+       "DELETE FROM cheby_namespaces WHERE namespace_name = '{}'".format(name))
+    connection.commit()
 
 
 def add_namespace(name, desc='', user=None):
-   if user is None:
-      usr = os.environ.get('USER', 'unknown')
-   else:
-      usr = user
-   cursor = connection.cursor()
-   cursor.execute(
-      """INSERT INTO cheby_namespaces
-          (namespace_name, namespace_description, creator)
-         VALUES('{}', '{}', '{}')""".format(name, desc, usr))
-   connection.commit()
+    if user is None:
+        usr = os.environ.get('USER', 'unknown')
+    else:
+        usr = user
+    cursor = connection.cursor()
+    cursor.execute(
+       """INSERT INTO cheby_namespaces
+           (namespace_name, namespace_description, creator)
+          VALUES('{}', '{}', '{}')""".format(name, desc, usr))
+    connection.commit()
+
 
 def cmd_add_namespace(cmd_name, *args):
-   if len(args) == 0 or len(args) > 3 :
-      sys.stderr.write(
-         'usage: {} namespace [DESCRIPTION] [CREATOR]\n'.format(cmd_name))
-      return
-   name = args[0]
-   desc = args[1] if len(args) > 1 else ''
-   user = args[2] if len(args) > 2 else None
-   add_namespace(name, desc, user)
+    if len(args) == 0 or len(args) > 3:
+        sys.stderr.write(
+           'usage: {} namespace [DESCRIPTION] [CREATOR]\n'.format(cmd_name))
+        return
+    name = args[0]
+    desc = args[1] if len(args) > 1 else ''
+    user = args[2] if len(args) > 2 else None
+    add_namespace(name, desc, user)
 
 
 def get_namespace_id(name):
-   cursor = connection.cursor()
-   cursor.execute(
-      """SELECT namespace_id
-         FROM cheby_namespaces
-         WHERE namespace_name = :name""", name=name)
-   rows = cursor.fetchall()
-   if len(rows) == 0:
-      return None
-   return rows[0][0]
-   
+    cursor = connection.cursor()
+    cursor.execute(
+       """SELECT namespace_id
+          FROM cheby_namespaces
+          WHERE namespace_name = :name""", name=name)
+    rows = cursor.fetchall()
+    if len(rows) == 0:
+        return None
+    return rows[0][0]
+
 
 def cmd_get_namespace_id(cmd_name, *args):
-   if len(args) != 1:
-      sys.stderr.write(
-         'usage: {} namespace\n'.format(cmd_name))
-      return
-   name = args[0]
-   nid = get_namespace_id(name)
-   print(nid)
+    if len(args) != 1:
+        sys.stderr.write('usage: {} namespace\n'.format(cmd_name))
+        return
+    name = args[0]
+    nid = get_namespace_id(name)
+    print(nid)
 
 
 def add_file(cmd_name, *args):
-   if len(args) != 2:
-      sys.stderr.write("usage: {} filename namespace\n".format(cmd_name))
-      return
-   filename = args[0]
-   namespace = args[1]
-   nid = get_namespace_id(namespace)
-   if nid is None:
-      sys.stderr.write("error: namespace '{}' not found\n".format(namespace))
-      return
-   content = open(filename).read()
-   cursor = connection.cursor()
-   cursor.execute(
-      """INSERT INTO cheby_files
-          (file_name, namespace_id, file_content)
-         VALUES(:name, :nid, :content)""",
-      name=filename,
-      nid=nid,
-      content=content)
-   connection.commit()
+    if len(args) != 2:
+        sys.stderr.write("usage: {} filename namespace\n".format(cmd_name))
+        return
+    filename = args[0]
+    namespace = args[1]
+    nid = get_namespace_id(namespace)
+    if nid is None:
+        sys.stderr.write("error: namespace '{}' not found\n".format(namespace))
+        return
+    content = open(filename).read()
+    cursor = connection.cursor()
+    cursor.execute(
+       """INSERT INTO cheby_files
+           (file_name, namespace_id, file_content)
+          VALUES(:name, :nid, :content)""",
+       name=filename,
+       nid=nid,
+       content=content)
+    connection.commit()
 
 
 def cat_file(cmd_name, *args):
-   if len(args) != 1:
-      sys.stderr.write("usage: {} filename\n".format(cmd_name))
-      return
-   filename = args[0]
-   cursor = connection.cursor()
-   cursor.execute(
-      """SELECT file_content FROM cheby_files
-         WHERE file_name = :name""", name=filename)
-   for content, in cursor.fetchall():
-      sys.stdout.write(content.read())
+    if len(args) != 1:
+        sys.stderr.write("usage: {} filename\n".format(cmd_name))
+        return
+    filename = args[0]
+    cursor = connection.cursor()
+    cursor.execute(
+       """SELECT file_content FROM cheby_files
+          WHERE file_name = :name""", name=filename)
+    for content, in cursor.fetchall():
+        sys.stdout.write(content.read())
+
 
 def add_include_by_id(cmd_name, *args):
-   if len(args) != 2:
-      sys.stderr.write("usage: {} file-id include-id\n".format(cmd_name))
-      return
-   fid = int(args[0])
-   iid = int(args[1])
-   cursor = connection.cursor()
-   cursor.execute(
-      """INSERT INTO cheby_file_inclusions
-          (source_file_id, included_file_id)
-         VALUES(:fid, :iid)""",
-      fid=fid, iid=iid)
-   connection.commit()
+    if len(args) != 2:
+        sys.stderr.write("usage: {} file-id include-id\n".format(cmd_name))
+        return
+    fid = int(args[0])
+    iid = int(args[1])
+    cursor = connection.cursor()
+    cursor.execute(
+        """INSERT INTO cheby_file_inclusions
+           (source_file_id, included_file_id)
+           VALUES(:fid, :iid)""",
+        fid=fid, iid=iid)
+    connection.commit()
+
 
 def extract_submaps(n):
-   """Return the list of all submaps contain in the hierarchy rooted by N"""
-   if isinstance(n, tree.Reg):
-      return []
-   elif isinstance(n, tree.Submap):
-      if n.filename is None:
-         return []
-      else:
-         return [n]
-   elif isinstance(n, tree.CompositeNode):
-      res = []
-      for e in n.children:
-         res.extend(extract_submaps(e))
-      return res
-   else:
-      raise AssertionError(n)
+    """Return the list of all submaps contain in the hierarchy rooted by N"""
+    if isinstance(n, tree.Reg):
+        return []
+    elif isinstance(n, tree.Submap):
+        if n.filename is None:
+            return []
+        else:
+            return [n]
+    elif isinstance(n, tree.CompositeNode):
+        res = []
+        for e in n.children:
+            res.extend(extract_submaps(e))
+        return res
+    else:
+        raise AssertionError(n)
+
 
 class DepEntry(object):
-   def __init__(self, filename):
-      self.filename = filename
-      self.includes = set()
-      self.node = None
-      self.fid = None
+    def __init__(self, filename):
+        self.filename = filename
+        self.includes = set()
+        self.node = None
+        self.fid = None
+
 
 def compute_fileset(filename):
-   deps = {}
-   # Start with the first file
-   todo = [filename]
-   while todo:
-      # Extract the first file from todo.
-      filename = todo[0]
-      todo = todo[1:]
-      # Skip it if already handled.
-      basename = os.path.basename(filename)
-      if basename in deps:
-         continue
-      # Add it to deps, load it.
-      de = DepEntry(filename)
-      deps[basename] = de
-      de.node = cheby.parser.parse_yaml(de.filename)
-      de.node.c_filename = filename
-      # Update todo with submaps.
-      submaps = extract_submaps(de.node)
-      for sm in submaps:
-         # Use absolute filename.
-         absname = cheby.layout.compute_submap_absolute_filename(sm)
-         todo.append(absname)
-         # Add it in the includes list of SM.
-         bn = os.path.basename(sm.filename)
-         de.includes.add(bn)
-         # Rename the submap filename.
-         sm.filename = basename
-   return deps
+    deps = {}
+    # Start with the first file
+    todo = [filename]
+    while todo:
+        # Extract the first file from todo.
+        filename = todo[0]
+        todo = todo[1:]
+        # Skip it if already handled.
+        basename = os.path.basename(filename)
+        if basename in deps:
+            continue
+        # Add it to deps, load it.
+        de = DepEntry(filename)
+        deps[basename] = de
+        de.node = cheby.parser.parse_yaml(de.filename)
+        de.node.c_filename = filename
+        # Update todo with submaps.
+        submaps = extract_submaps(de.node)
+        for sm in submaps:
+            # Use absolute filename.
+            absname = cheby.layout.compute_submap_absolute_filename(sm)
+            todo.append(absname)
+            # Add it in the includes list of SM.
+            bn = os.path.basename(sm.filename)
+            de.includes.add(bn)
+            # Rename the submap filename.
+            sm.filename = basename
+    return deps
+
 
 def show_file_include(cmd_name, *args):
-   if len(args) != 1:
-      sys.stderr.write("usage: {} FILE\n".format(cmd_name))
-      return
-   deps = compute_fileset(args[0])
-   for filename, de in deps.items():
-      print("{}: ({})".format(filename, de.filename))
-      for f in de.includes:
-         print ("  {}".format(f))
+    if len(args) != 1:
+        sys.stderr.write("usage: {} FILE\n".format(cmd_name))
+        return
+    deps = compute_fileset(args[0])
+    for filename, de in deps.items():
+        print("{}: ({})".format(filename, de.filename))
+        for f in de.includes:
+            print ("  {}".format(f))
+
 
 def upload_files(cmd_name, *args):
-   if len(args) != 2:
-      sys.stderr.write("usage: {} FILE NAMESPACE\n".format(cmd_name))
-      return
-   filename = args[0]
-   namespace = args[1]
-   nid = get_namespace_id(namespace)
-   if nid is not None:
-      sys.stderr.write("namespace '{}' already used\n".format(namespace))
-      return
-   deps = compute_fileset(filename)
-   # Create namespace
-   add_namespace(namespace)
-   nid = get_namespace_id(namespace)
-   assert nid is not None
-   cursor = connection.cursor()
-   # Add files
-   for f, de in deps.items():
-      # Create id
-      cursor.execute("SELECT cheby_seq.nextval from dual")
-      rows = cursor.fetchall()
-      assert len(rows) == 1
-      de.fid = rows[0][0]
-      # Add file
-      buf = Buffer()
-      pprint.pprint_cheby(buf, de.node)
-      cursor.execute(
-         """INSERT INTO cheby_files
-            (file_id, file_name, namespace_id, file_content)
-            VALUES(:fid, :name, :nid, :content)""",
-         fid=de.fid,
-         name=f,
-         nid=nid,
-         content=buf.buffer)
-   connection.commit()
-      
+    if len(args) != 2:
+        sys.stderr.write("usage: {} FILE NAMESPACE\n".format(cmd_name))
+        return
+    filename = args[0]
+    namespace = args[1]
+    nid = get_namespace_id(namespace)
+    if nid is not None:
+        sys.stderr.write("namespace '{}' already used\n".format(namespace))
+        return
+    deps = compute_fileset(filename)
+    # Create namespace
+    add_namespace(namespace)
+    nid = get_namespace_id(namespace)
+    assert nid is not None
+    cursor = connection.cursor()
+    # Add files
+    for f, de in deps.items():
+        # Create id
+        cursor.execute("SELECT cheby_seq.nextval from dual")
+        rows = cursor.fetchall()
+        assert len(rows) == 1
+        de.fid = rows[0][0]
+        # Add file
+        buf = Buffer()
+        pprint.pprint_cheby(buf, de.node)
+        cursor.execute(
+           """INSERT INTO cheby_files
+              (file_id, file_name, namespace_id, file_content)
+              VALUES(:fid, :name, :nid, :content)""",
+           fid=de.fid,
+           name=f,
+           nid=nid,
+           content=buf.buffer)
+    connection.commit()
+
 
 def empty_namespace(cmd_name, *args):
-   if len(args) != 1:
-      sys.stderr.write("usage: {} NAMESPACE\n".format(cmd_name))
-      return
-   namespace = args[0]
-   cursor = connection.cursor()
-   cursor.execute(
+    if len(args) != 1:
+        sys.stderr.write("usage: {} NAMESPACE\n".format(cmd_name))
+        return
+    namespace = args[0]
+    cursor = connection.cursor()
+    cursor.execute(
       """DELETE FROM cheby_files
          WHERE namespace_id = (
            SELECT ns.namespace_id
            FROM cheby_namespaces ns
            WHERE ns.namespace_name = :name)""",
       name=namespace)
-   connection.commit()
+    connection.commit()
+
 
 def download_namespace(cmd_name, *args):
-   if len(args) != 1:
-      sys.stderr.write("usage: {} NAMESPACE\n".format(cmd_name))
-      return
-   namespace = args[0]
-   cursor = connection.cursor()
-   cursor.execute(
+    if len(args) != 1:
+        sys.stderr.write("usage: {} NAMESPACE\n".format(cmd_name))
+        return
+    namespace = args[0]
+    cursor = connection.cursor()
+    cursor.execute(
       """SELECT file_name, file_content FROM cheby_files
          WHERE namespace_id = (
            SELECT ns.namespace_id
            FROM cheby_namespaces ns
            WHERE ns.namespace_name = :name)""",
       name=namespace)
-   for filename, content in cursor.fetchall():
-      print("writing {}".format(filename))
-      with open(filename, "w") as f:
-         f.write(content.read())
+    for filename, content in cursor.fetchall():
+        print("writing {}".format(filename))
+        with open(filename, "w") as f:
+            f.write(content.read())
 
 
 def cmd_help(cmd_name, *args):
-   print("list of commands:")
-   for k in commands:
-      print(" {}".format(k))
+    print("list of commands:")
+    for k in commands:
+        print(" {}".format(k))
+
 
 commands = {
-   'show': show,
-   'help': cmd_help,
-   'list-namespaces': list_namespaces,
-   'remove-namespace': remove_namespace,
-   'add-namespace': cmd_add_namespace,
-   'get-namespace-id': cmd_get_namespace_id,
-   'list-filenames': list_filenames,
-   'add-file': add_file,
-   'cat-file': cat_file,
-   'list-includes': list_includes,
-   'add-include-by-id': add_include_by_id,
-   'show-file-include': show_file_include,
-   'upload-files': upload_files,
-   'empty-namespace': empty_namespace,
-   'download-namespace': download_namespace
+    'show': show,
+    'help': cmd_help,
+    'list-namespaces': list_namespaces,
+    'remove-namespace': remove_namespace,
+    'add-namespace': cmd_add_namespace,
+    'get-namespace-id': cmd_get_namespace_id,
+    'list-filenames': list_filenames,
+    'add-file': add_file,
+    'cat-file': cat_file,
+    'list-includes': list_includes,
+    'add-include-by-id': add_include_by_id,
+    'show-file-include': show_file_include,
+    'upload-files': upload_files,
+    'empty-namespace': empty_namespace,
+    'download-namespace': download_namespace
 }
 
+
 def bad_cmd(cmd_name, *args):
-   sys.stderr.write("Unknown command '{}', try --help\n".format(cmd_name))
-   sys.exit(1)
+    sys.stderr.write("Unknown command '{}', try --help\n".format(cmd_name))
+    sys.exit(1)
 
 
 def main():
-   global connection
+    global connection
 
-   parser = argparse.ArgumentParser(description='CCDB/Cheby bridge')
-   parser.add_argument("-c", dest='config', required=True,
-                       help='DB connection (usr/passwd@dsn)')
-   parser.add_argument("args", nargs='+')
-   args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='CCDB/Cheby bridge')
+    parser.add_argument("-c", dest='config', required=True,
+                        help='DB connection (usr/passwd@dsn)')
+    parser.add_argument("args", nargs='+')
+    args = parser.parse_args()
 
-   connection = cx_Oracle.connect(args.config)
+    connection = cx_Oracle.connect(args.config)
 
-   cmd_name = args.args[0]
-   cmd_args = args.args[1:]                       
-   proc = commands.get(cmd_name, bad_cmd)
-   proc(cmd_name, *cmd_args)
+    cmd_name = args.args[0]
+    cmd_args = args.args[1:]
+    proc = commands.get(cmd_name, bad_cmd)
+    proc(cmd_name, *cmd_args)
 
 
 if __name__ == '__main__':
-   main()
+    main()
