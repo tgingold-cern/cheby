9c0c00f7d5b0d3acaff98b3c1574feff238dd91d
Pieter Van Trappen <pieter.van.trappen@cern.ch>
2018-11-19 08:18:51 +0100

    gen-custom: removed custom code, upon calling a local gen_custom.py will be ran if existing

--- proto/cheby/gen_custom.py
+++ proto/cheby/gen_custom.py
@@ -1,30 +1,11 @@
-import cheby.tree as tree
-
-def gen_macro(fd, root, acc, prefix, middle, suffix):
-    for r in root.children:
-        if isinstance(r, tree.Reg):
-            if r.access == acc:
-                if 'set' in prefix:
-                    fd.write('{}{}{}{}{}'.format(
-                        prefix, r.name, middle, r.c_address, suffix))
-                else:
-                    fd.write('{}{}{}{}{}'.format(
-                        prefix, r.c_address, middle, r.name, suffix))
+import os
 
 def generate_custom(fd, root):
-    setpref="block->set"
-    setmid="(fasec->read_reg("
-    setsuff="));\\\n"
-    getpref="fasec->write_reg("
-    getmid="_,block->get"
-    getsuff="());\\\n"
-
-    fd.write('//read functions ro data\n')
-    fd.write('#define {}_read_ro \\\n'.format(root.name))
-    gen_macro(fd, root, 'ro', setpref, setmid, setsuff)
-    fd.write('\n//read functions rw data\n')
-    fd.write('#define {}_read_rw \\\n'.format(root.name))
-    gen_macro(fd, root, 'rw', setpref, setmid, setsuff)
-    fd.write('\n//write functions rw data\n')
-    fd.write('#define {}_write_rw \\\n'.format(root.name))
-    gen_macro(fd, root, 'rw', getpref, getmid, getsuff)
+    """Opens the gen_custom.py file from the invoking directory and runs generate_custom(fd, root)
+    """
+    fn = os.getcwd()+"/gen_custom.py"
+    if os.path.isfile(fn):
+        exec(open(fn).read(), globals())
+        generate_custom(fd,root)
+    else:
+        print("Error: file %s not found" % fn)
