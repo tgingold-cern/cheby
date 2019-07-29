309b84789da94094d909f5304a0f1c17f7d945da
Tristan Gingold <tristan.gingold@cern.ch>
2019-07-29 14:36:32 +0200

    integration: remove old version.

--- integration/cern-ci.sh
+++ integration/cern-ci.sh
@@ -10,9 +10,19 @@ python -V
 [ x"$CI_COMMIT_SHORT_SHA" != x ] || exit 1
 
 base_destdir=/acc/local/share/ht_tools/noarch/cheby
-destdir=$base_destdir/cheby-$CI_COMMIT_SHORT_SHA
+suffix=$CI_COMMIT_SHORT_SHA
+destdir=$base_destdir/cheby-$suffix
 prefix=$destdir/lib/python3.6/site-packages/
 mkdir -p $prefix
 
 export PYTHONPATH=$PYTHONPATH:$prefix
 python3 ./setup.py install --prefix $destdir
+
+cd $base_destdir
+ln -sfn cheby-$suffix cheby-latest
+
+if [ -f last ]; then
+    old=$(cat last)
+    rm -rf ./cheby-$old
+fi
+echo $suffix last
