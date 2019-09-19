e6868ffdbac6782404809351701dfe72b6eb26af
Tristan Gingold <tristan.gingold@cern.ch>
2019-09-19 17:09:30 +0200

    Add testcase from issue #24

--- proto/tests.py
+++ proto/tests.py
@@ -218,7 +218,7 @@ def test_hdl():
               'inter-mt/mt_cpu_xb-busgroup.cheby',
               'inter-mt/mt_cpu_xb-busgroup2.cheby',
               'inter-mt/mt_cpu_lr-busgroup.cheby',
-              'demo_all.cheby', 'big_addr.cheby']:
+              'demo_all.cheby', 'big_addr.cheby', 'issue24/map_arrays.cheby']:
         if verbose:
             print('test hdl: {}'.format(f))
         t = parse_ok(srcdir + f)
