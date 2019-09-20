e7dee07b4a4347057441cdbb60eef1d73bc38782
Tristan Gingold <tristan.gingold@cern.ch>
2019-09-20 16:22:23 +0200

    Add a testcase for cern-info constants.

--- proto/tests.py
+++ proto/tests.py
@@ -218,7 +218,8 @@ def test_hdl():
               'inter-mt/mt_cpu_xb-busgroup.cheby',
               'inter-mt/mt_cpu_xb-busgroup2.cheby',
               'inter-mt/mt_cpu_lr-busgroup.cheby',
-              'demo_all.cheby', 'big_addr.cheby', 'issue24/map_arrays.cheby']:
+              'demo_all.cheby', 'big_addr.cheby', 'issue24/map_arrays.cheby',
+              'cern_info.cheby']:
         if verbose:
             print('test hdl: {}'.format(f))
         t = parse_ok(srcdir + f)
