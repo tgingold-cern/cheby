26b8c6026717c1bd0b14193189604f30fbc0dabe
Tristan Gingold <tristan.gingold@cern.ch>
2019-12-06 13:41:14 +0100

    Add schemas_version.py file.

---
+++ proto/cheby/schemas_version.py
@@ -0,0 +1,10 @@
+VERSIONS = {
+    'core': "1.0.0",
+    'x-gena': "1.0.0",
+    'x-hdl': "1.0.0",
+    'x-fesa': "1.0.0",
+    'x-driver-edge': "1.0.0",
+    'x-conversions': "1.0.0",
+    'x-enums': "1.0.0",
+    "x-wbgen": "1.0.0"
+}
