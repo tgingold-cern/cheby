f544901c24fc3190cab9087fe4e856143b845158
Tristan Gingold <tristan.gingold@cern.ch>
2019-12-13 14:20:06 +0100

    testfiles: add a test for enums.

--- proto/tests.py
+++ proto/tests.py
@@ -277,7 +277,7 @@ def test_hdl_ref():
               'issue41/bugBlockFields',
               'issue45/test8', 'issue45/test16',
               'features/wires1', 'features/semver1', 'features/semver2',
-              'features/mapinfo2',
+              'features/mapinfo2', 'features/enums1',
               'issue52/hwInfo']:
         if verbose:
             print('test hdl with ref: {}'.format(f))
---
+++ testfiles/features/enums1.vhdl
@@ -0,0 +1,139 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package enums1_pkg is
+
+  -- Enumeration enum1
+  constant C_enum1_hello : std_logic_vector(7 downto 0) := "00000000";
+  constant C_enum1_World : std_logic_vector(7 downto 0) := "00000001";
+
+  -- Enumeration enum2
+  constant C_enum2_hello : std_logic_vector(0 downto 0) := "0";
+  constant C_enum2_world : std_logic_vector(0 downto 0) := "1";
+end enums1_pkg;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.enums1_pkg.all;
+
+entity enums1 is
+  port (
+    rst_n_i              : in    std_logic;
+    clk_i                : in    std_logic;
+    wb_cyc_i             : in    std_logic;
+    wb_stb_i             : in    std_logic;
+    wb_sel_i             : in    std_logic_vector(3 downto 0);
+    wb_we_i              : in    std_logic;
+    wb_dat_i             : in    std_logic_vector(31 downto 0);
+    wb_ack_o             : out   std_logic;
+    wb_err_o             : out   std_logic;
+    wb_rty_o             : out   std_logic;
+    wb_stall_o           : out   std_logic;
+    wb_dat_o             : out   std_logic_vector(31 downto 0);
+
+    -- REG r1
+    r1_o                 : out   std_logic_vector(31 downto 0)
+  );
+end enums1;
+
+architecture syn of enums1 is
+  signal rd_req_int                     : std_logic;
+  signal wr_req_int                     : std_logic;
+  signal rd_ack_int                     : std_logic;
+  signal wr_ack_int                     : std_logic;
+  signal wb_en                          : std_logic;
+  signal ack_int                        : std_logic;
+  signal wb_rip                         : std_logic;
+  signal wb_wip                         : std_logic;
+  signal r1_reg                         : std_logic_vector(31 downto 0);
+  signal r1_wreq                        : std_logic;
+  signal r1_wack                        : std_logic;
+  signal rd_ack_d0                      : std_logic;
+  signal rd_dat_d0                      : std_logic_vector(31 downto 0);
+  signal wr_req_d0                      : std_logic;
+  signal wr_dat_d0                      : std_logic_vector(31 downto 0);
+  signal wr_sel_d0                      : std_logic_vector(3 downto 0);
+begin
+
+  -- WB decode signals
+  wb_en <= wb_cyc_i and wb_stb_i;
+
+  process (clk_i) begin
+    if rising_edge(clk_i) then
+      if rst_n_i = '0' then
+        wb_rip <= '0';
+      else
+        wb_rip <= (wb_rip or (wb_en and not wb_we_i)) and not rd_ack_int;
+      end if;
+    end if;
+  end process;
+  rd_req_int <= (wb_en and not wb_we_i) and not wb_rip;
+
+  process (clk_i) begin
+    if rising_edge(clk_i) then
+      if rst_n_i = '0' then
+        wb_wip <= '0';
+      else
+        wb_wip <= (wb_wip or (wb_en and wb_we_i)) and not wr_ack_int;
+      end if;
+    end if;
+  end process;
+  wr_req_int <= (wb_en and wb_we_i) and not wb_wip;
+
+  ack_int <= rd_ack_int or wr_ack_int;
+  wb_ack_o <= ack_int;
+  wb_stall_o <= not ack_int and wb_en;
+  wb_rty_o <= '0';
+  wb_err_o <= '0';
+
+  -- pipelining for wr-in+rd-out
+  process (clk_i) begin
+    if rising_edge(clk_i) then
+      if rst_n_i = '0' then
+        rd_ack_int <= '0';
+        wr_req_d0 <= '0';
+      else
+        rd_ack_int <= rd_ack_d0;
+        wb_dat_o <= rd_dat_d0;
+        wr_req_d0 <= wr_req_int;
+        wr_dat_d0 <= wb_dat_i;
+        wr_sel_d0 <= wb_sel_i;
+      end if;
+    end if;
+  end process;
+
+  -- Register r1
+  r1_o <= r1_reg;
+  process (clk_i) begin
+    if rising_edge(clk_i) then
+      if rst_n_i = '0' then
+        r1_reg <= "00000000000000000000000000000000";
+        r1_wack <= '0';
+      else
+        if r1_wreq = '1' then
+          r1_reg <= wr_dat_d0;
+        end if;
+        r1_wack <= r1_wreq;
+      end if;
+    end if;
+  end process;
+
+  -- Process for write requests.
+  process (wr_req_d0, r1_wack) begin
+    r1_wreq <= '0';
+    -- Reg r1
+    r1_wreq <= wr_req_d0;
+    wr_ack_int <= r1_wack;
+  end process;
+
+  -- Process for read requests.
+  process (rd_req_int, r1_reg) begin
+    -- By default ack read requests
+    rd_dat_d0 <= (others => 'X');
+    -- Reg r1
+    rd_ack_d0 <= rd_req_int;
+    rd_dat_d0 <= r1_reg;
+  end process;
+end syn;
