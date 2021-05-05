31bb198f628bfa66bbe49708023a2ad8e50935c5
Tristan Gingold <tristan.gingold@cern.ch>
2021-05-05 14:19:14 +0200

    Bump version

--- proto/cheby/__init__.py
+++ proto/cheby/__init__.py
@@ -1,1 +1,7 @@
-__version__ = '1.4.dev0'
+# Version
+# Should be:
+# 1.4.dev0 for development
+# 1.4.rc12 for candidate
+# 1.4.0    for release
+# 1.5.dev0 for development of the next release.
+__version__ = '1.4.rc1'
