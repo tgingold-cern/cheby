20ef1d2df2c6fd49ff912e0f0270b95da3ac1f78
Tristan Gingold <tristan.gingold@cern.ch>
2021-09-15 15:39:43 +0200

    gen_c.py: correctly pad repeat blocks.  For issue #99

--- proto/cheby/gen_c.py
+++ proto/cheby/gen_c.py
@@ -84,6 +84,13 @@ def cprint_children(cp, n, size):
             # between the real submap and how much memory is used
             # in this map.
             addr = el.c_address + el.c_submap.c_size
+        elif isinstance(el, tree.Repeat):
+            # There might be a gap between the size of the repeat block,
+            # and the size of the element * count.
+            rep_size = el.c_elsize * el.count
+            addr = el.c_address + rep_size
+            maybe_pad(el.c_size - rep_size)
+            addr = el.c_address + el.c_size
         else:
             addr = el.c_address + el.c_size
     # Last pad
