ce05e9ca10942dd5650990ed2384b2ca910374b0
Tristan Gingold <tristan.gingold@cern.ch>
2021-10-29 10:58:28 +0200

    integration: adjust prefix directory

--- integration/cern-ci.sh
+++ integration/cern-ci.sh
@@ -6,6 +6,8 @@ set -e
 # Use the right python tool.
 . /acc/local/share/python/acc-py/base/pro/setup.sh
 python -V
+pyver=$(python -c 'import sys; print("{}.{}".format(sys.version_info.major, sys.version_info.minor))')
+echo "python version: $pyver"
 
 # Check the variable is set
 [ x"$CI_COMMIT_SHORT_SHA" != x ] || exit 1
@@ -19,7 +21,7 @@ else
     suffix=$CI_COMMIT_SHORT_SHA
 fi
 destdir=$base_destdir/cheby-$suffix
-prefix=$destdir/lib/python3.6/site-packages/
+prefix=$destdir/lib/python${pyver}/site-packages/
 mkdir -p $prefix
 
 export PYTHONPATH=$PYTHONPATH:$prefix
