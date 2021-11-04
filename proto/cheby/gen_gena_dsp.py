e2734af881d1140bdf17d3553fddd0bda4ea2fa8
Tristan Gingold <tristan.gingold@cern.ch>
2021-11-04 08:52:10 +0100

    gen_gena_dsp: factorize code

--- proto/cheby/gen_gena_dsp.py
+++ proto/cheby/gen_gena_dsp.py
@@ -9,38 +9,7 @@ ACC = {'ro': 'r  ', 'wo': 'w  ', 'rw': 'rw '}
 def get_gena(n, name, default=None):
     return n.get_extension('x_gena', name, default)
 
-
 class Printer(tree.Visitor):
-    def __init__(self, fd, root):
-        self.fd = fd
-        self.addr = 0x02000000
-        self.offset = 0
-        self.root = root
-        self.word_size = root.c_word_size
-        self.prefix = ''
-        self.prefix_stack = []
-
-    def mp_push(self, name, addr):
-        self.prefix_stack.append((self.prefix, self.offset))
-        self.prefix = self.prefix + name + '_'
-        self.offset += addr
-
-    def mp_pop(self):
-        self.prefix, self.offset = self.prefix_stack.pop()
-
-    def mp_raw(self, s):
-        self.fd.write(s)
-
-    def mp_txt(self, txt):
-        self.mp_raw(txt)
-        self.mp_raw('\n')
-
-    def mp_addr(self, n, comment):
-        addr = self.addr | ((self.offset + n.c_address) // self.word_size)
-        self.mp_txt('#define {:50s} (0x{:08X}){}'.format(self.prefix + n.name, addr, comment))
-
-
-class HPrinter(tree.Visitor):
     def __init__(self, fd, root):
         self.fd = fd
         self.root = root
@@ -64,6 +33,23 @@ class HPrinter(tree.Visitor):
         self.pr_raw('\n')
 
 
+class MPrinter(Printer):
+    def __init__(self, fd, root):
+        super(MPrinter, self).__init__(fd, root)
+        self.addr = 0x02000000
+        self.offset = 0
+        self.word_size = root.c_word_size
+
+    def mp_addr(self, n, comment):
+        addr = self.addr | ((self.offset + n.c_address) // self.word_size)
+        self.pr_txt('#define {:50s} (0x{:08X}){}'.format(self.prefix + n.name, addr, comment))
+
+
+class HPrinter(Printer):
+    def __init__(self, fd, root):
+        super(HPrinter, self).__init__(fd, root)
+
+
 def mprint_children(pr, n):
     for el in n.c_sorted_children:
         pr.visit(el)
@@ -82,9 +68,9 @@ def mprint_field(mp, n, f):
     else:
         comment = 'sub-reg'
         mask = (2 << f.hi) - mask
-    mp.mp_txt('#define {:50s} (0x{:08X}) // {}'.format(
+    mp.pr_txt('#define {:50s} (0x{:08X}) // {}'.format(
         pfx, mask, comment))
-    mp.mp_txt('#define {:50s} ({}) // {} shift poZzition'.format(
+    mp.pr_txt('#define {:50s} ({}) // {} shift poZzition'.format(
         pfx + 'ShiftPoz', f.lo, comment))
     mprint_enum(mp, f, pfx)
 
@@ -95,7 +81,7 @@ def mprint_enum(mp, n, pfx):
         return
     enums = mp.root.c_enums_dict[enum]
     for e in enums.children:
-        mp.mp_txt('#define {:50s} {} // code-field'.format(pfx + '_' + e.name, e.value))
+        mp.pr_txt('#define {:50s} {} // code-field'.format(pfx + '_' + e.name, e.value))
 
 
 def hprint_enum(pr, n):
@@ -147,11 +133,11 @@ def hprint_field(pr, n, f):
     hprint_enum(pr, f)
 
 
-@Printer.register(tree.Reg)
+@MPrinter.register(tree.Reg)
 def mprint_reg(mp, n):
     typ = get_ctype(n)
     if typ is None:
-        mp.mp_txt('//not implemented ctype for {}: bit_encoding = {}, el_width = {}'.format(
+        mp.pr_txt('//not implemented ctype for {}: bit_encoding = {}, el_width = {}'.format(
             mp.prefix + n.name, 'unsigned', n.c_size * tree.BYTE_SIZE))
     else:
         if get_gena(n, 'rmw', False):
@@ -178,11 +164,11 @@ def hprint_reg(pr, n):
         hprint_field(pr, n, f)
 
 
-@Printer.register(tree.Block)
+@MPrinter.register(tree.Block)
 def mprint_block(mp, n):
-    mp.mp_push(n.name, n.c_address)
+    mp.pr_push(n.name, n.c_address)
     mprint_children(mp, n)
-    mp.mp_pop()
+    mp.pr_pop()
 
 
 @HPrinter.register(tree.Block)
@@ -192,7 +178,7 @@ def hprint_block(pr, n):
     pr.pr_pop()
 
 
-@Printer.register(tree.Memory)
+@MPrinter.register(tree.Memory)
 def mprint_memory(mp, n):
     acc = ACC[n.children[0].access]
     mp.mp_addr(n, '// memory-data, {:3s}, {}'.format(acc, 'unknown type'))
@@ -204,11 +190,11 @@ def hprint_memory(pr, n):
         pr.pr_txt('//not_implemented: {}etter of a memory-data element'.format(c))
 
 
-@Printer.register(tree.Submap)
+@MPrinter.register(tree.Submap)
 def mprint_submap(mp, n):
-    mp.mp_push(n.name, 0)
+    mp.pr_push(n.name, 0)
     mprint_children(mp, n.c_submap)
-    mp.mp_pop()
+    mp.pr_pop()
 
 
 @HPrinter.register(tree.Submap)
@@ -219,14 +205,14 @@ def hprint_submap(pr, n):
 
 
 def gen_gena_dsp_map(fd, root, with_date=True):
-    mp = Printer(fd, root)
+    mp = MPrinter(fd, root)
     if with_date:
         ts = datetime.datetime.now()    
-        mp.mp_txt('#define GENERATED_ON (0x{0:08X})  // generation time, format: hex(yyyymmdd)'.format(int(ts.strftime("%Y%m%d"))))
+        mp.pr_txt('#define GENERATED_ON (0x{0:08X})  // generation time, format: hex(yyyymmdd)'.format(int(ts.strftime("%Y%m%d"))))
     version = get_gena(root, 'map-version')
     if version is not None:
-        mp.mp_txt("#define MEMMAP_VERSION (0x{:08X}) // memory map version, format: hex(yyyymmdd)".format(int(version)))
-    mp.mp_txt('')
+        mp.pr_txt("#define MEMMAP_VERSION (0x{:08X}) // memory map version, format: hex(yyyymmdd)".format(int(version)))
+    mp.pr_txt('')
     mprint_children(mp, root)
 
 
