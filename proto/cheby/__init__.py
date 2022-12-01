8d9b11aed6a396fad6545d8977c40138b85d43ae
Dimitris Lampridis <dimitris.lampridis@cern.ch>
2022-12-01 10:48:19 +0100

    Bump version to 1.6.dev0

--- proto/cheby/__init__.py
+++ proto/cheby/__init__.py
@@ -1,7 +1,7 @@
 # Version
 # Should be:
-# 1.4.dev0 for development
-# 1.4.rc12 for candidate
-# 1.4.0    for release
-# 1.5.dev0 for development of the next release.
-__version__ = '1.5.0'
+# 1.6.dev0 for development
+# 1.6.rc12 for candidate
+# 1.6.0    for release
+# 1.7.dev0 for development of the next release.
+__version__ = '1.6.dev0'
