9d3051506c73cf5b4fe6a726adb32f92f49814e2
Tristan Gingold <tristan.gingold@cern.ch>
2024-02-26 14:14:15 +0100

    testfiles: add missing baselines

---
+++ testfiles/features/memwide_ua-consts.sv
@@ -0,0 +1,11 @@
+package memwide_ua_Consts;
+  localparam MEMWIDE_UA_SIZE = 256;
+  localparam ADDR_MEMWIDE_UA_REGA = 'h0;
+  localparam MEMWIDE_UA_REGA_FIELD0_OFFSET = 1;
+  localparam MEMWIDE_UA_REGA_FIELD0 = 32'h2;
+  localparam ADDR_MEMWIDE_UA_TS = 'h80;
+  localparam MEMWIDE_UA_TS_SIZE = 16;
+  localparam ADDR_MEMWIDE_UA_TS_RISE_SEC = 'h0;
+  localparam ADDR_MEMWIDE_UA_TS_RISE_NS = 'h4;
+  localparam ADDR_MEMWIDE_UA_TS_FALL_SEC = 'h8;
+endpackage
---
+++ testfiles/features/memwide_ua-consts.vhdl
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+package memwide_ua_Consts is
+  constant MEMWIDE_UA_SIZE : Natural := 256;
+  constant ADDR_MEMWIDE_UA_REGA : Natural := 16#0#;
+  constant MEMWIDE_UA_REGA_FIELD0_OFFSET : Natural := 1;
+  constant ADDR_MEMWIDE_UA_TS : Natural := 16#80#;
+  constant MEMWIDE_UA_TS_SIZE : Natural := 16;
+  constant ADDR_MEMWIDE_UA_TS_RISE_SEC : Natural := 16#0#;
+  constant ADDR_MEMWIDE_UA_TS_RISE_NS : Natural := 16#4#;
+  constant ADDR_MEMWIDE_UA_TS_FALL_SEC : Natural := 16#8#;
+end package memwide_ua_Consts;
