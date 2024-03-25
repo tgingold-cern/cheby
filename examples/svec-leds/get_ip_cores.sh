227a586c45a4d5f0ec122b9d4858580d291c91a6
Ilja Bekman (zelxil) <i.bekman@fz-juelich.de>
2024-03-25 19:07:48 +0100

    remove submodules; add script to recreate IPs

---
+++ examples/svec-leds/get_ip_cores.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# to use this example following IPs should be cloned
+git clone https://ohwr.org/hdl-core-lib/general-cores.git ./ip_cores/general-cores
+git clone https://ohwr.org/hdl-core-lib/vme64x-core.git ./ip_cores/vme64x-core
+git clone https://ohwr.org/hdl-core-lib/general-cores.git ./ip_cores/vme64x-core/hdl/ip_cores/general-cores
+
