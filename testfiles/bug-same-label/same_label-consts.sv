9e1f0b22d07fc170e4ff41e527d654604965024f
Lorenz Schmid <lorenzschmid@users.noreply.github.com>
2024-07-02 10:04:05 +0200

    Update golden files

--- testfiles/bug-same-label/same_label-consts.sv
+++ testfiles/bug-same-label/same_label-consts.sv
@@ -3,13 +3,16 @@ package same_label_reg_Consts;
   localparam ADDR_SAME_LABEL_REG_NO_FIELDS = 'h0;
   localparam SAME_LABEL_REG_NO_FIELDS_PRESET = 8'h20;
   localparam ADDR_SAME_LABEL_REG_SAME_NAME = 'h4;
+  localparam SAME_LABEL_REG_SAME_NAME_WIDTH = 1;
   localparam SAME_LABEL_REG_SAME_NAME_OFFSET = 0;
   localparam SAME_LABEL_REG_SAME_NAME = 32'h1;
   localparam ADDR_SAME_LABEL_REG_SAME_NAME_MULTI = 'h8;
+  localparam SAME_LABEL_REG_SAME_NAME_MULTI_WIDTH = 12;
   localparam SAME_LABEL_REG_SAME_NAME_MULTI_OFFSET = 0;
   localparam SAME_LABEL_REG_SAME_NAME_MULTI = 32'hfff;
   localparam ADDR_SAME_LABEL_REG_NOT_SAME_REG = 'hc;
   localparam ADDR_SAME_LABEL_REG_NOT_SAME = 'hc;
+  localparam SAME_LABEL_REG_NOT_SAME_WIDTH = 1;
   localparam SAME_LABEL_REG_NOT_SAME_OFFSET = 0;
   localparam SAME_LABEL_REG_NOT_SAME = 32'h1;
 endpackage
--- testfiles/features/memwide_ua-consts.sv
+++ testfiles/features/memwide_ua-consts.sv
@@ -2,6 +2,7 @@ package memwide_ua_Consts;
   localparam MEMWIDE_UA_SIZE = 256;
   localparam ADDR_MEMWIDE_UA_REGA = 'h0;
   localparam ADDR_MEMWIDE_UA_REGA_FIELD0 = 'h0;
+  localparam MEMWIDE_UA_REGA_FIELD0_WIDTH = 1;
   localparam MEMWIDE_UA_REGA_FIELD0_OFFSET = 1;
   localparam MEMWIDE_UA_REGA_FIELD0 = 32'h2;
   localparam ADDR_MEMWIDE_UA_TS = 'h80;
