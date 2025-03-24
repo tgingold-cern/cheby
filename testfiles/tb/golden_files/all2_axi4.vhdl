a6952dfeaf1cc5c734e31f1e0b54fe292811854b
Pieter Van Trappen <pieter.van.trappen@cern.ch>
2025-03-24 12:21:00 +0100

    axi4litebus busgroup - update tb golden_files due to existing busgroup attribute in most cheby files

--- testfiles/tb/golden_files/all2_axi4.vhdl
+++ testfiles/tb/golden_files/all2_axi4.vhdl
@@ -1,54 +1,21 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.axi4lite_pkg.all;
 
 entity all2_axi4 is
   port (
+    axi4l_i              : in    t_axi4lite_subordinate_in;
+    axi4l_o              : out   t_axi4lite_subordinate_out;
     aclk                 : in    std_logic;
     areset_n             : in    std_logic;
-    awvalid              : in    std_logic;
-    awready              : out   std_logic;
-    awaddr               : in    std_logic_vector(6 downto 2);
-    awprot               : in    std_logic_vector(2 downto 0);
-    wvalid               : in    std_logic;
-    wready               : out   std_logic;
-    wdata                : in    std_logic_vector(31 downto 0);
-    wstrb                : in    std_logic_vector(3 downto 0);
-    bvalid               : out   std_logic;
-    bready               : in    std_logic;
-    bresp                : out   std_logic_vector(1 downto 0);
-    arvalid              : in    std_logic;
-    arready              : out   std_logic;
-    araddr               : in    std_logic_vector(6 downto 2);
-    arprot               : in    std_logic_vector(2 downto 0);
-    rvalid               : out   std_logic;
-    rready               : in    std_logic;
-    rdata                : out   std_logic_vector(31 downto 0);
-    rresp                : out   std_logic_vector(1 downto 0);
 
     -- REG reg1
     reg1_o               : out   std_logic_vector(31 downto 0);
 
     -- AXI-4 lite bus sub2_axi4
-    sub2_axi4_awvalid_o  : out   std_logic;
-    sub2_axi4_awready_i  : in    std_logic;
-    sub2_axi4_awaddr_o   : out   std_logic_vector(5 downto 2);
-    sub2_axi4_awprot_o   : out   std_logic_vector(2 downto 0);
-    sub2_axi4_wvalid_o   : out   std_logic;
-    sub2_axi4_wready_i   : in    std_logic;
-    sub2_axi4_wdata_o    : out   std_logic_vector(31 downto 0);
-    sub2_axi4_wstrb_o    : out   std_logic_vector(3 downto 0);
-    sub2_axi4_bvalid_i   : in    std_logic;
-    sub2_axi4_bready_o   : out   std_logic;
-    sub2_axi4_bresp_i    : in    std_logic_vector(1 downto 0);
-    sub2_axi4_arvalid_o  : out   std_logic;
-    sub2_axi4_arready_i  : in    std_logic;
-    sub2_axi4_araddr_o   : out   std_logic_vector(5 downto 2);
-    sub2_axi4_arprot_o   : out   std_logic_vector(2 downto 0);
-    sub2_axi4_rvalid_i   : in    std_logic;
-    sub2_axi4_rready_o   : out   std_logic;
-    sub2_axi4_rdata_i    : in    std_logic_vector(31 downto 0);
-    sub2_axi4_rresp_i    : in    std_logic_vector(1 downto 0)
+    sub2_axi4_i          : in    t_axi4lite_manager_in;
+    sub2_axi4_o          : out   t_axi4lite_manager_out
   );
 end all2_axi4;
 
@@ -84,9 +51,9 @@ architecture syn of all2_axi4 is
 begin
 
   -- AW, W and B channels
-  awready <= not axi_awset;
-  wready <= not axi_wset;
-  bvalid <= axi_wdone;
+  axi4l_o.awready <= not axi_awset;
+  axi4l_o.wready <= not axi_wset;
+  axi4l_o.bvalid <= axi_wdone;
   process (aclk) begin
     if rising_edge(aclk) then
       if areset_n = '0' then
@@ -96,21 +63,21 @@ begin
         axi_wdone <= '0';
       else
         wr_req <= '0';
-        if awvalid = '1' and axi_awset = '0' then
-          wr_addr <= awaddr;
+        if axi4l_i.awvalid = '1' and axi_awset = '0' then
+          wr_addr <= axi4l_i.awaddr(6 downto 2);
           axi_awset <= '1';
           wr_req <= axi_wset;
         end if;
-        if wvalid = '1' and axi_wset = '0' then
-          wr_data <= wdata;
-          wr_sel(7 downto 0) <= (others => wstrb(0));
-          wr_sel(15 downto 8) <= (others => wstrb(1));
-          wr_sel(23 downto 16) <= (others => wstrb(2));
-          wr_sel(31 downto 24) <= (others => wstrb(3));
+        if axi4l_i.wvalid = '1' and axi_wset = '0' then
+          wr_data <= axi4l_i.wdata;
+          wr_sel(7 downto 0) <= (others => axi4l_i.wstrb(0));
+          wr_sel(15 downto 8) <= (others => axi4l_i.wstrb(1));
+          wr_sel(23 downto 16) <= (others => axi4l_i.wstrb(2));
+          wr_sel(31 downto 24) <= (others => axi4l_i.wstrb(3));
           axi_wset <= '1';
-          wr_req <= axi_awset or awvalid;
+          wr_req <= axi_awset or axi4l_i.awvalid;
         end if;
-        if (axi_wdone and bready) = '1' then
+        if (axi_wdone and axi4l_i.bready) = '1' then
           axi_wset <= '0';
           axi_awset <= '0';
           axi_wdone <= '0';
@@ -121,37 +88,37 @@ begin
       end if;
     end if;
   end process;
-  bresp <= "00";
+  axi4l_o.bresp <= "00";
 
   -- AR and R channels
-  arready <= not axi_arset;
-  rvalid <= axi_rdone;
+  axi4l_o.arready <= not axi_arset;
+  axi4l_o.rvalid <= axi_rdone;
   process (aclk) begin
     if rising_edge(aclk) then
       if areset_n = '0' then
         rd_req <= '0';
         axi_arset <= '0';
         axi_rdone <= '0';
-        rdata <= (others => '0');
+        axi4l_o.rdata <= (others => '0');
       else
         rd_req <= '0';
-        if arvalid = '1' and axi_arset = '0' then
-          rd_addr <= araddr;
+        if axi4l_i.arvalid = '1' and axi_arset = '0' then
+          rd_addr <= axi4l_i.araddr(6 downto 2);
           axi_arset <= '1';
           rd_req <= '1';
         end if;
-        if (axi_rdone and rready) = '1' then
+        if (axi_rdone and axi4l_i.rready) = '1' then
           axi_arset <= '0';
           axi_rdone <= '0';
         end if;
         if rd_ack = '1' then
           axi_rdone <= '1';
-          rdata <= rd_data;
+          axi4l_o.rdata <= rd_data;
         end if;
       end if;
     end if;
   end process;
-  rresp <= "00";
+  axi4l_o.rresp <= "00";
 
   -- pipelining for wr-in+rd-out
   process (aclk) begin
@@ -190,31 +157,31 @@ begin
   end process;
 
   -- Interface sub2_axi4
-  sub2_axi4_awvalid_o <= sub2_axi4_aw_val;
-  sub2_axi4_awaddr_o <= wr_adr_d0(5 downto 2);
-  sub2_axi4_awprot_o <= "000";
-  sub2_axi4_wvalid_o <= sub2_axi4_w_val;
-  sub2_axi4_wdata_o <= wr_dat_d0;
+  sub2_axi4_o.awvalid <= sub2_axi4_aw_val;
+  sub2_axi4_o.awaddr <= ((25 downto 0 => '0') & wr_adr_d0(5 downto 2)) & (1 downto 0 => '0');
+  sub2_axi4_o.awprot <= "000";
+  sub2_axi4_o.wvalid <= sub2_axi4_w_val;
+  sub2_axi4_o.wdata <= wr_dat_d0;
   process (wr_sel_d0) begin
-    sub2_axi4_wstrb_o <= (others => '0');
+    sub2_axi4_o.wstrb <= (others => '0');
     if not (wr_sel_d0(7 downto 0) = (7 downto 0 => '0')) then
-      sub2_axi4_wstrb_o(0) <= '1';
+      sub2_axi4_o.wstrb(0) <= '1';
     end if;
     if not (wr_sel_d0(15 downto 8) = (7 downto 0 => '0')) then
-      sub2_axi4_wstrb_o(1) <= '1';
+      sub2_axi4_o.wstrb(1) <= '1';
     end if;
     if not (wr_sel_d0(23 downto 16) = (7 downto 0 => '0')) then
-      sub2_axi4_wstrb_o(2) <= '1';
+      sub2_axi4_o.wstrb(2) <= '1';
     end if;
     if not (wr_sel_d0(31 downto 24) = (7 downto 0 => '0')) then
-      sub2_axi4_wstrb_o(3) <= '1';
+      sub2_axi4_o.wstrb(3) <= '1';
     end if;
   end process;
-  sub2_axi4_bready_o <= '1';
-  sub2_axi4_arvalid_o <= sub2_axi4_ar_val;
-  sub2_axi4_araddr_o <= rd_addr(5 downto 2);
-  sub2_axi4_arprot_o <= "000";
-  sub2_axi4_rready_o <= '1';
+  sub2_axi4_o.bready <= '1';
+  sub2_axi4_o.arvalid <= sub2_axi4_ar_val;
+  sub2_axi4_o.araddr <= ((25 downto 0 => '0') & rd_addr(5 downto 2)) & (1 downto 0 => '0');
+  sub2_axi4_o.arprot <= "000";
+  sub2_axi4_o.rready <= '1';
   process (aclk) begin
     if rising_edge(aclk) then
       if areset_n = '0' then
@@ -222,15 +189,15 @@ begin
         sub2_axi4_w_val <= '0';
         sub2_axi4_ar_val <= '0';
       else
-        sub2_axi4_aw_val <= sub2_axi4_wr or (sub2_axi4_aw_val and not sub2_axi4_awready_i);
-        sub2_axi4_w_val <= sub2_axi4_wr or (sub2_axi4_w_val and not sub2_axi4_wready_i);
-        sub2_axi4_ar_val <= sub2_axi4_rd or (sub2_axi4_ar_val and not sub2_axi4_arready_i);
+        sub2_axi4_aw_val <= sub2_axi4_wr or (sub2_axi4_aw_val and not sub2_axi4_i.awready);
+        sub2_axi4_w_val <= sub2_axi4_wr or (sub2_axi4_w_val and not sub2_axi4_i.wready);
+        sub2_axi4_ar_val <= sub2_axi4_rd or (sub2_axi4_ar_val and not sub2_axi4_i.arready);
       end if;
     end if;
   end process;
 
   -- Process for write requests.
-  process (wr_adr_d0, wr_req_d0, reg1_wack, sub2_axi4_bvalid_i) begin
+  process (wr_adr_d0, wr_req_d0, reg1_wack, sub2_axi4_i.bvalid) begin
     reg1_wreq <= '0';
     sub2_axi4_wr <= '0';
     case wr_adr_d0(6 downto 6) is
@@ -246,14 +213,14 @@ begin
     when "1" =>
       -- Submap sub2_axi4
       sub2_axi4_wr <= wr_req_d0;
-      wr_ack <= sub2_axi4_bvalid_i;
+      wr_ack <= sub2_axi4_i.bvalid;
     when others =>
       wr_ack <= wr_req_d0;
     end case;
   end process;
 
   -- Process for read requests.
-  process (rd_addr, rd_req, reg1_reg, sub2_axi4_rdata_i, sub2_axi4_rvalid_i) begin
+  process (rd_addr, rd_req, reg1_reg, sub2_axi4_i.rdata, sub2_axi4_i.rvalid) begin
     -- By default ack read requests
     rd_dat_d0 <= (others => 'X');
     sub2_axi4_rd <= '0';
@@ -270,8 +237,8 @@ begin
     when "1" =>
       -- Submap sub2_axi4
       sub2_axi4_rd <= rd_req;
-      rd_dat_d0 <= sub2_axi4_rdata_i;
-      rd_ack_d0 <= sub2_axi4_rvalid_i;
+      rd_dat_d0 <= sub2_axi4_i.rdata;
+      rd_ack_d0 <= sub2_axi4_i.rvalid;
     when others =>
       rd_ack_d0 <= rd_req;
     end case;
