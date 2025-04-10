9498bf94af37bbd4fbfa623525343077c62f1987
Clement Viken Zrounba <clement.zrounba@cern.ch>
2025-04-10 13:47:12 +0200

    Add tests for !67 fix

---
+++ testfiles/mr67/top-consts.sv
@@ -0,0 +1,7 @@
+package fid_top_axi_Consts;
+  localparam FID_TOP_AXI_MEMMAP_VERSION = 'h100;
+  localparam ADDR_FID_TOP_AXI_IP = 'h0;
+  localparam ADDR_MASK_FID_TOP_AXI_IP = 'h0;
+  localparam ADDR_FMASK_FID_TOP_AXI_IP = 'h0;
+  localparam FID_TOP_AXI_IP_SIZE = 4;
+endpackage
