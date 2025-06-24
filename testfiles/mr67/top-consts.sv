5f551d9593ebc246147c94be8145b0b3b44bdf11
Lorenz Schmid <lorenzschmid@users.noreply.github.com>
2025-06-24 11:43:54 +0200

    Fix packaging names (fit them to generated version)

--- testfiles/mr67/top-consts.sv
+++ testfiles/mr67/top-consts.sv
@@ -1,4 +1,4 @@
-package fid_top_axi_Consts;
+package pkg_fid_top_axi_consts;
   localparam FID_TOP_AXI_MEMMAP_VERSION = 'h100;
   localparam ADDR_FID_TOP_AXI_IP = 'h0;
   localparam ADDR_MASK_FID_TOP_AXI_IP = 'h0;
