215c969dd7e076ce9945375adb156a83205d489a
Lorenz Schmid <lorenzschmid@users.noreply.github.com>
2025-08-12 08:34:31 +0200

    Generate HTML tables instead AsciiDoc table for Markdown documentation

--- proto/cheby/print_markdown.py
+++ proto/cheby/print_markdown.py
@@ -10,68 +10,76 @@ def format_text(text):
     return text.replace("\n", " +\n")
 
 
-def print_reg(fd, r, abs_addr, hide_comments=False):
-    wln(fd, "[horizontal]")
-    wln(fd, "HDL name:: {}".format(r.c_name))
-    wln(fd, "address:: 0x{:x}".format(abs_addr))
-    wln(fd, "block offset:: 0x{:x}".format(r.c_address))
-    wln(fd, "access mode:: {}".format(r.access))
+def print_reg(fd, r, name, abs_addr, hide_comments=False):
+    # Description of Register
+    wln(fd, "### Register: {}".format(name))
+    wln(fd)
 
     if r.description:
-        wln(fd)
         wln(fd, format_text(r.description))
-
-    if r.comment:
         wln(fd)
+
+    if r.comment and not hide_comments:
         wln(fd, format_text(r.comment))
+        wln(fd)
+
+    wln(fd, "- **HDL name**: {}".format(r.c_name))
+    wln(fd, "- **Address**: 0x{:x}".format(abs_addr))
+    wln(fd, "- **Block Offset**: 0x{:x}".format(r.c_address))
+    wln(fd, "- **Access Mode**: {}".format(r.access))
 
     wln(fd)
-    descr = gen_doc.build_regdescr_table(r)
-    wln(fd, '[cols="8*^"]')
-    wln(fd, "|===")
-    for desc_raw in descr:
-        wln(fd)
-        for col in desc_raw:
-            style = ""
-            if col.colspan > 1:
-                style += "{}+".format(col.colspan)
-            if col.style == 'field':
-                style += 's'
-            wln(fd, "{}| {}".format(style, col.content))
-    wln(fd, "|===")
 
+    # Table of Bit Fields (HTML format)
+    descr = gen_doc.build_regdescr_table(r)
+    wln(fd, "<table>")
+    for desc_raw in descr:
+        wln(fd, "  <tr>")
+        for col in desc_raw:
+            if col.style == "bit":
+                wln(fd, "    <td><b>{}</b></td>".format(col.content))
+            else:
+                attrs = ""
+                if col.colspan > 1:
+                    attrs = ' colspan="{}" style="text-align: center;"'.format(
+                        col.colspan
+                    )
+
+                wln(fd, "    <td{}>{}</td>".format(attrs, col.content))
+
+        wln(fd, "  </tr>")
+
+    wln(fd, "</table>")
+    wln(fd)
+
+    # Description of Bit Fields (HTML format)
     if r.has_fields():
-        wln(fd)
         for f in r.children:
-            wln(fd, "{}::".format(f.name))
+            wln(fd, "#### Bit: {}".format(f.name))
+            wln(fd)
 
             not_documented = True
+
             if f.description:
                 wln(fd, format_text(f.description))
+                wln(fd)
                 not_documented = False
 
             if f.comment and not hide_comments:
-                if f.description:
-                    wln(fd, "+")
                 wln(fd, format_text(f.comment))
+                wln(fd)
                 not_documented = False
 
             if not_documented:
-                wln(fd, "(not documented)")
-
-        wln(fd)
+                wln(fd, "_(not documented)_")
+                wln(fd)
 
 
 def print_map_summary(fd, summary):
-    wln(fd, "|===")
-    wln(fd, "|HW address | Type | Name | HDL name")
+    wln(fd, "| HW address | Type | Name | HDL Name |")
+    wln(fd, "|------------|------|------|----------|")
     for r in summary.raws:
-        wln(fd)
-        wln(fd, "|{}".format(r.address))
-        wln(fd, "|{}".format(r.typ))
-        wln(fd, "|{}".format(r.name))
-        wln(fd, "|{}".format(r.node.c_name))
-    wln(fd, "|===")
+        wln(fd, "| {} | {} | {} | {} |".format(r.address, r.typ, r.name, r.node.c_name))
     wln(fd)
 
 
@@ -79,30 +87,35 @@ def print_reg_description(fd, summary, hide_comments=False):
     for ra in summary.raws:
         r = ra.node
         if isinstance(r, tree.Reg):
-            wln(fd, "=== {}".format(ra.name))
-            print_reg(fd, r, ra.abs_addr, hide_comments)
+            print_reg(fd, r, ra.name, ra.abs_addr, hide_comments)
 
 
 def print_root(fd, root, hide_comments=False):
-    wln(fd, "== Memory map summary")
-    wln(fd, root.description or '(no description)')
-    wln(fd)
+    wln(fd, "## Memory Map Summary")
+
+    if root.description:
+        wln(fd, root.description)
+        wln(fd)
+
     if root.version is not None:
-        wln(fd, "version: {}".format(root.version))
+        wln(fd, "Version: {}".format(root.version))
         wln(fd)
 
     if root.c_address_spaces_map is None:
         summary = gen_doc.MemmapSummary(root)
         print_map_summary(fd, summary)
-        wln(fd, "== Registers description")
+
+        wln(fd, "## Registers Description")
         print_reg_description(fd, summary, hide_comments)
+
     else:
         summaries = [(gen_doc.MemmapSummary(space), space) for space in root.children]
         for summary, space in summaries:
-            wln(fd, "== For space {}".format(space.name))
+            wln(fd, "## For Space {}".format(space.name))
             print_map_summary(fd, summary)
+
         for summary, space in summaries:
-            wln(fd, "== Registers description for space {}\n".format(space.name))
+            wln(fd, "## Registers Description for Space {}\n".format(space.name))
             print_reg_description(fd, summary, hide_comments)
 
 
